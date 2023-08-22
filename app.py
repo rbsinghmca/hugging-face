f865782ee018ea7dd0ec87247d7bb5715012e572
rbsinghmca <50779409+rbsinghmca@users.noreply.github.com>
2023-08-22 16:16:31 +0530

    app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def predict(prompt):
     return summary
 
 with gr.Blocks() as demo:
-    textbox = gr.Textbox(placeholder="Enter text to summarize", lines=4)
-    gr.Interface(fn=predict, inputs=textbox, outputs=text)
+    textbox = gr.Textbox(placeholder="Enter text block to summarize", lines=4)
+    gr.Interface(fn=predict, inputs=textbox, outputs="text")
 
 demo.launch()
